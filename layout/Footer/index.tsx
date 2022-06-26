e7c0e54b4f9c2b0437e046701ce84a2be1e1ec20
solohad1n <fox663399@gmail.com>
2022-06-26 20:20:23 +0600

    Added: 1. grid markup. 2. Footer is stylized

--- layout/Footer/index.tsx
+++ layout/Footer/index.tsx
@@ -1,7 +1,14 @@
 import { IFooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: IFooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: IFooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)}{...props}>
+      <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href="#" target="_blank" rel='noopenervnoreferrer'>Пользовательское соглашение</a>
+      <a href="#" target="_blank" rel='noopenervnoreferrer'>Политика конфиденциальности</a>
+    </footer>
+  )
 };
